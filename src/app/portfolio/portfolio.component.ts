88c69818b79edb00c5e18603e9f33d0e5ed73c16
Mario Gutiérrez <marioguti033@gmail.com>
2024-06-23 12:55:23 +0200

    cambios portfolio

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,25 +1,45 @@
+  import { CommonModule } from '@angular/common';
 import { Component, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+  import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-portfolio',
-  standalone: true,
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
-})
-export class PortfolioComponent {
-  elementos = [
-    { 
-      titulo: 'Elemento 1',
-      descripcion: 'Descripción del elemento 1',
-      etiquetas: ['etiqueta1', 'etiqueta2']
-    },
-    { 
-      titulo: 'Elemento 2',
-      descripcion: 'Descripción del elemento 2',
-      etiquetas: ['etiqueta3', 'etiqueta4']
-    },
-    // Agrega más elementos según sea necesario
-  ];
-}
+  @Component({
+    selector: 'app-portfolio',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './portfolio.component.html',
+    styleUrl: './portfolio.component.scss'
+  })
+  export class PortfolioComponent {
+    proyectos = [
+      { 
+        titulo: 'Elemento 1',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore',
+        etiquetas: ['etiqueta1', 'etiqueta2']
+      },
+      { 
+        titulo: 'Elemento 2',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore' ,
+        etiquetas: ['etiqueta3', 'etiqueta4']
+      },
+      {
+        titulo: 'Elemento 3',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore',
+        etiquetas: ['etiqueta5', 'etiqueta6']
+      },
+      { 
+        titulo: 'Elemento 1',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore',
+        etiquetas: ['etiqueta1', 'etiqueta2']
+      },
+      { 
+        titulo: 'Elemento 2',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore' ,
+        etiquetas: ['etiqueta3', 'etiqueta4']
+      },
+      {
+        titulo: 'Elemento 3',
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore',
+        etiquetas: ['etiqueta5', 'etiqueta6']
+      }
+    ];
+  }
